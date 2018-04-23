19f60aa82ccda036402f486d2854a61372a8e9d3
bestjsguru <dlid3991@gmail.com>
2018-04-22 22:10:23 -0400

    feat: implemented the detail page

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { PagesComponent } from './pages.component';
+import { PatientDetailComponent } from './patient-detail/patient-detail.component';
 
 const routes: Routes = [
-  { 
+  {
     path: '',
     component: PagesComponent,
     children: [
       { path: 'summaries', loadChildren: './summaries/summaries.module#SummariesModule' },
+      {
+        path: 'patient-detail/:id',
+        component: PatientDetailComponent,
+        pathMatch: 'full'
+      },
       { path: '', redirectTo: 'summaries' },
       { path: '**', redirectTo: 'summaries' },
     ]
